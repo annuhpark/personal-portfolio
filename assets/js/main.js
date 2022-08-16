41f16caefbf7a268c28e58324b7b0ecbf86ee299
Anna Park <AnnaPark.Developer@gmail.com>
2022-08-16 22:03:07 +0000

    Fixed arrows in carousel.

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,15 +95,15 @@ modalCloses.forEach(modalClose => {
 /* ==================== PORTFOLIO SWIPER  ==================== */
 var swiper = new Swiper('.mySwiper', {
   cssMode: true,
+  loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   },
   pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true
+    el: '.swiper-pagination',
+    clickable: true
+  }
 });
 
 /* ==================== TESTIMONIAL ==================== */

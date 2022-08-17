9b4072ac8612a90604886f76653dd151ff58a559
Anna Park <AnnaPark.Developer@gmail.com>
2022-08-17 20:23:14 +0000

    In Progress.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,55 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
+// Email Form Submission
+
+var form = document.querySelector('.my-form');
+// var button = document.getElementById("my-form-button");
+var status = document.querySelector('.status-text');
+
+window.addEventListener('DOMContentLoaded', function () {
+  // get the form elements defined in your form HTML above
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    // status.classList.add('success');
+    status.innerHTML = 'Thanks!';
+    console.log(status);
+  }
+
+  function error() {
+    // status.classList.add('error');
+    status.innerHTML = 'Oops! There was a problem.';
+  }
+
+  // handle the form submission event
+
+  form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
 /* ===== MENU SHOW ===== */
 /* Validate if constant exists */
 if (navToggle) {
